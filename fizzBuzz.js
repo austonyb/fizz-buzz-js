6930bf952e6477711306dac725b296d6b0a34b46
austonyb <auston.youngblood@gmail.com>
2022-09-29 10:12:17 -0600

    Added fibonacci sequence generator

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,16 +1,26 @@
 //fizzbuzz
 
-for (i = 0; i <= 50; i++){
-    if((i % 3 === 0) && (i % 5 === 0)){
-        console.log("fizzbuzz")
-    } else if (i % 5 === 0){
-        console.log("buzz")
-    } else if (i % 3 === 0){
-        console.log("fizz")
-    } else {
-        console.log(i)
-    }
-  }
+// for (i = 0; i <= 50; i++){
+//     if((i % 3 === 0) && (i % 5 === 0)){
+//         console.log("fizzbuzz")
+//     } else if (i % 5 === 0){
+//         console.log("buzz")
+//     } else if (i % 3 === 0){
+//         console.log("fizz")
+//     } else {
+//         console.log(i)
+//     }
+//   }
 
+  //fibonacci
 
-  
+let n1 = 0, n2 = 1, nextTerm;
+
+let number = 5
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
