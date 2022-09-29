06e263e9cb37b0b3acef9f0f3866d9f3ee68cc4e
austonyb <auston.youngblood@gmail.com>
2022-09-29 09:37:38 -0600

    Solved Fizz Buzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,8 +1,16 @@
 //fizzbuzz
 
 for (i = 0; i <= 50; i++){
-    console.log(i)
+    if((i % 3 === 0) && (i % 5 === 0)){
+        console.log("fizzbuzz")
+    } else if (i % 5 === 0){
+        console.log("buzz")
+    } else if (i % 3 === 0){
+        console.log("fizz")
+    } else {
+        console.log(i)
+    }
   }
 
-  
+
   
